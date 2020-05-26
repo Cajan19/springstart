0f3bc230518373509bb5d10214831c46f8561445
Carolin Janshen <caro.janshen@gmail.com>
2020-05-26 07:47:13 +0200

    Add Studentdb files

---
+++ src/main/java/de/neuefische/studentdbweb/StudentdbApplication.java
@@ -0,0 +1,22 @@
+package de.neuefische.studentdbweb;
+
+import de.neuefische.studentdbweb.model.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class StudentdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentdbApplication.class, args);
+
+
+
+
+
+
+    }
+}
